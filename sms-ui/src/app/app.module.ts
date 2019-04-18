136acace69dc2cfc54e1491611d2194800526dd6
Robert Connell <robert.lel.connell@gmail.com>
2019-04-17 21:58:14 -0400

    new files

--- sms-ui/src/app/app.module.ts
+++ sms-ui/src/app/app.module.ts
@@ -8,26 +8,17 @@ import { AuthLoadingComponent } from './components/auth-loading/auth-loading.com
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CognitoService } from './services/cognito.service';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from './navbar/navbar.component';
-=======
 import { SmsClientModule } from './sms-client/sms-client.module';
 
 
->>>>>>> dc432b0b495464c297f74c3fec09af8d7bd4a09c
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-    NavbarComponent
-=======
     HomeComponent,
     AuthLoadingComponent
->>>>>>> dc432b0b495464c297f74c3fec09af8d7bd4a09c
   ],
   imports: [
     BrowserModule,
