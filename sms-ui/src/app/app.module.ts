57a763748ce219eac2a3c4c1998551053bd6b34e
JordanPonder <JordanPonder@protonmail.ch>
2019-04-17 21:43:35 -0400

    there is a message

--- sms-ui/src/app/app.module.ts
+++ sms-ui/src/app/app.module.ts
@@ -8,33 +8,36 @@ import { AuthLoadingComponent } from './components/auth-loading/auth-loading.com
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CognitoService } from './services/cognito.service';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from './navbar/navbar.component';
-=======
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SmsClientModule } from './sms-client/sms-client.module';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
 
 
->>>>>>> dc432b0b495464c297f74c3fec09af8d7bd4a09c
 
+const routes: Routes = [
+  { path: 'profileInfo', component: ProfileInfoComponent },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-    NavbarComponent
-=======
+    NavbarComponent,
+    ProfileInfoComponent,
     HomeComponent,
     AuthLoadingComponent
->>>>>>> dc432b0b495464c297f74c3fec09af8d7bd4a09c
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    SmsClientModule
+    SmsClientModule, AppRoutingModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [
     CognitoService,
