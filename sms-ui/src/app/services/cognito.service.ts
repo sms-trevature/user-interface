9a600d681c6f473bf4459cf6ed66e669c8490391
EllenZheng <zhengyingdi@gmail.com>
2019-04-21 16:18:23 -0400

    stash pop

--- sms-ui/src/app/services/cognito.service.ts
+++ sms-ui/src/app/services/cognito.service.ts
@@ -25,14 +25,19 @@ export class CognitoService {
   public token$ = this.tokenStream.asObservable();
 
   private newPasswordUser: any = null;
-
-  constructor() {
+//private grabUserJson:
+  constructor( ) {
     this.setup();
   }
 
   async login(username: string, password: string) {
     const response = await Auth.signIn(username, password);
+    ///---Query by email for user info.. 
+    console.log(' email: ' + username); 
+    
+
     console.log(response);
+    this.getCurrent(); 
 
     if (response.challengeName !== 'NEW_PASSWORD_REQUIRED') {
       this.setup();
@@ -47,8 +52,12 @@ export class CognitoService {
 
   async logout() {
     await Auth.signOut();
+   
     this.tokenStream.next(undefined);
     this.currentUserStream.next(undefined);
+  }
+  async getCurrent(){
+     
   }
 
   async setNewPassword(password: string) {
