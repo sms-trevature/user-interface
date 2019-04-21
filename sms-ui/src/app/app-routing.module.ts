1350b9340c8b9871a0727d10bfd1e5c17fa566c9
EllenZheng <zhengyingdi@gmail.com>
2019-04-21 15:48:40 -0400

    discard changes for later merge

--- sms-ui/src/app/app-routing.module.ts
+++ sms-ui/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthLoadingComponent } from './components/auth-loading/auth-loading.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { SurveyCreatorComponent } from './components/survey/survey-creator/survey-creator.component';
+import { SurveyListComponent } from './components/survey/survey-list/survey-list.component';
 
 const routes: Routes = [
   {
@@ -16,7 +18,16 @@ const routes: Routes = [
   {
     path: 'auth-loading',
     component: AuthLoadingComponent
+  },
+  {
+    path: 'survey',
+    component: SurveyCreatorComponent
+  },
+  {
+    path: 'surveyList',
+    component: SurveyListComponent
   }
+
 ];
 
 @NgModule({
