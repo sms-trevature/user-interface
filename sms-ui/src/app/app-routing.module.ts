1e35e2771358406f6c17fe8ebeb59d64b7680c11
gfgonzalez <47013456+gfgonzalez@users.noreply.github.com>
2019-04-22 14:19:07 -0400

    Revert "Interview dev"

--- sms-ui/src/app/app-routing.module.ts
+++ sms-ui/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthLoadingComponent } from './components/auth-loading/auth-loading.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { InterviewListComponent } from './components/interview-list/interview-list.component';
 
 import { SurveyCreatorComponent } from './components/survey/survey-creator/survey-creator.component';
 import { SurveyListComponent } from './components/survey/survey-list/survey-list.component';
@@ -26,10 +25,7 @@ const routes: Routes = [
     component: AuthLoadingComponent
   },
   {
-    path: 'interview-list',
-    component: InterviewListComponent
-  },
-  {
+
     path: 'survey',
     component: SurveyCreatorComponent
   },
