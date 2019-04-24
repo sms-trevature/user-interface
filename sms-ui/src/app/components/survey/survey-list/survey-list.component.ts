b24fbc613f0328253dab961bd999816ebc4a07f6
EllenZheng <zhengyingdi@gmail.com>
2019-04-24 10:13:51 -0400

    pull from development

--- sms-ui/src/app/components/survey/survey-list/survey-list.component.ts
+++ sms-ui/src/app/components/survey/survey-list/survey-list.component.ts
@@ -5,6 +5,8 @@ import { SurveyQuestionService } from 'src/app/sms-client/clients/surveyquestion
 import { SurveyQuestion, Question } from 'src/app/sms-client/dto/surveyQuestion';
 import { Answer } from 'src/app/sms-client/dto/Answer';
 import { SurveyAnswerService } from 'src/app/sms-client/clients/survey-answer.service';
+import { Responses } from 'src/app/sms-client/dto/Response';
+import { SurveyResponseService } from 'src/app/sms-client/clients/survey-response.service';
 
 @Component({
   selector: 'app-survey-list',
@@ -16,9 +18,12 @@ export class SurveyListComponent implements OnInit {
   listOfSurvey: Survey[];
   curTemplate: SurveyQuestion[];
   curTempAnswers: Array<Answer[]>;
+  ArrayOfResponseAnswerList: Array<string[]>;
+  arrOfCounts: Array<number[]>;
   constructor(private surveyService: SurveyService,
               private sqService: SurveyQuestionService,
-              private answerService: SurveyAnswerService) {}
+              private answerService: SurveyAnswerService,
+              private responseService: SurveyResponseService) {}
 
   ngOnInit() {
     this.surveyService.findAll().subscribe(
@@ -35,25 +40,55 @@ export class SurveyListComponent implements OnInit {
     this.surveyService.closeSurvey(this.listOfSurvey[index]).subscribe(
       data => {
         this.listOfSurvey[index] = data;
+        console.log(this.listOfSurvey[index]);
       }
     );
   }
 
   checkTemplate(surveyId: number) {
+    this.ArrayOfResponseAnswerList = [];
     this.curTemplate = [];
-    this.curTempAnswers = [];
     this.sqService.getTemplate(surveyId).subscribe(
       data => {
         this.curTemplate = data;
+        this.curTempAnswers = new Array(data.length);
 // tslint:disable-next-line: forin
         for (const i in data) {
           this.answerService.findByQuestionId(data[i].questionId.questionId).subscribe(
             curQuestionAnswerList => {
               this.curTempAnswers[i] = curQuestionAnswerList;
-              console.log(curQuestionAnswerList);
             }
           );
         }
+      }
+    );
+  }
+
+  getGraph(surveyId: number) {
+    this.curTemplate = [];
+    this.responseService.findBySurveyId(surveyId).subscribe(
+      data => {
+        let temp = [data[0].answerId.answer];
+        let count = [1];
+        for (let i = 0; i < data.length - 1; i++) {
+          if (!data[i] || !data[i].answerId) { continue; }
+          if (data[i].answerId.questionId === data[i + 1].answerId.questionId) {
+
+            if (data[i].answerId.id !== data[i + 1].answerId.id) {
+              temp.push(data[i + 1].answerId.answer);
+              count.push(1);
+            } else {
+              count[count.length - 1]++;
+            }
+          } else {
+            this.ArrayOfResponseAnswerList.push(temp);
+            this.arrOfCounts.push(count);
+            count = [1];
+            temp = [data[i + 1].answerId.answer];
+          }
+        }
+        console.log(this.ArrayOfResponseAnswerList);
+        console.log(this.arrOfCounts);
       }
     );
   }
