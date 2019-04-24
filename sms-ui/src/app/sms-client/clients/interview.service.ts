3f6f25e2116bc73922efbdf862f0bd9ed81c2e76
astone2007 <astone2007@gmail.com>
2019-04-24 14:50:32 -0400

    Adding more search and sorting functionality for interview list

--- sms-ui/src/app/sms-client/clients/interview.service.ts
+++ sms-ui/src/app/sms-client/clients/interview.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Interview } from './interview';
+import { Interview } from '../dto/Interview';
 
 @Injectable({
   providedIn: 'root'
