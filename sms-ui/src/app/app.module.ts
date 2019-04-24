23056c8c9b1218616cb1ea84770e86b7466d8fc6
DebbieBiteye <debbiebiteye@gmail.com>
2019-04-24 11:20:56 -0400

    debbie2

--- sms-ui/src/app/app.module.ts
+++ sms-ui/src/app/app.module.ts
@@ -45,6 +45,7 @@ import { AnswerData7Component } from './components/survey/survey-data/graph-comp
 
 
 import { StagingMgrFeedbackComponent } from './components/interview/staging-mgr-feedback/staging-mgr-feedback.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 
 
@@ -130,7 +131,8 @@ const routes: Routes = [
     FormsModule,
     ChartsModule,
     SmsClientModule, AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BsDatepickerModule.forRoot()
 
   ],
   providers: [
