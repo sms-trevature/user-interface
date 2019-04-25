e0ffb61c947c8f92c55313e436c63ba080fa57b5
Stephon Montgomery II <Stephonlm2@gmail.com>
2019-04-25 15:08:10 -0400

    deleting redaundant code

--- sms-ui/src/app/app.module.ts
+++ sms-ui/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { ChartsModule } from 'ng2-charts';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDatabaseService } from 'src/app/services/Inmemorydb/in-memory-db.service';
 import { AppComponent } from './app.component';
 import { AuthLoadingComponent } from './components/auth-loading/auth-loading.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CognitoService } from './services/cognito.service';
-
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SmsClientModule } from './sms-client/sms-client.module';
 import { InterviewListComponent } from './components/interview-list/interview-list.component';
@@ -20,7 +16,6 @@ import { NewInterviewComponent } from './components/new-interview/new-interview.
 import { AssociateFeedbackFormComponent } from './components/associate-feedback-form/associate-feedback-form.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
-
 import { ReportsComponent } from './reports/reports.component';
 import { InterviewComponent } from './interview/interview.component';
 import { ManageComponent } from './manage/manage.component';
@@ -28,9 +23,7 @@ import { MngrSubAssociatesComponent } from './mngr-sub-associates/mngr-sub-assoc
 import { SubMan2CohortComponent } from './sub-man2-cohort/sub-man2-cohort.component';
 import { FakeServiceComponent } from './fake-service/fake-service.component';
 import { SurveyCreatorComponent } from './components/survey/survey-creator/survey-creator.component';
-
 import { CohortModalComponent } from './components/cohortModal/cohort-modal.component';
-
 import { SurveyListComponent } from './components/survey/survey-list/survey-list.component';
 import { TimestampPipe } from './pipe/timestamp.pipe';
 import { SurveyGridComponent } from './components/survey/survey-grid/survey-grid.component';
@@ -38,17 +31,12 @@ import { AnswerData2Component } from './components/survey/survey-data/graph-comp
 import { AnswerData1Component } from './components/survey/survey-data/graph-components/answer-data1/answer-data1.component';
 import { SurveyDataComponent } from './components/survey/survey-data/survey-data.component';
 import { AssignSurveyComponent } from './components/survey/assign-survey/assign-survey.component';
-
 import { AnswerData3Component } from './components/survey/survey-data/graph-components/answer-data3/answer-data3.component';
 import { AnswerData4Component } from './components/survey/survey-data/graph-components/answer-data4/answer-data4.component';
 import { AnswerData5Component } from './components/survey/survey-data/graph-components/answer-data5/answer-data5.component';
 import { AnswerData6Component } from './components/survey/survey-data/graph-components/answer-data6/answer-data6.component';
 import { AnswerData7Component } from './components/survey/survey-data/graph-components/answer-data7/answer-data7.component';
-
-
 import { StagingMgrFeedbackComponent } from './components/interview/staging-mgr-feedback/staging-mgr-feedback.component';
-
-
 
 
 const routes: Routes = [
@@ -98,7 +86,6 @@ const routes: Routes = [
     ReportsComponent,
     InterviewComponent,
     ManageComponent,
-
     MngrSubAssociatesComponent,
     SubMan2CohortComponent,
     FakeServiceComponent,
@@ -107,12 +94,9 @@ const routes: Routes = [
     AssociateFeedbackFormComponent,
     SurveyCreatorComponent,
     CohortModalComponent,
-
-
     SurveyCreatorComponent,
     SurveyListComponent,
     TimestampPipe,
-
     SurveyDataComponent,
     AnswerData1Component,
     AnswerData2Component,
@@ -122,9 +106,6 @@ const routes: Routes = [
     AnswerData5Component,
     AnswerData6Component,
     AnswerData7Component,
-
-
-
   ],
   imports: [
     BrowserModule,
@@ -132,10 +113,6 @@ const routes: Routes = [
     NgbModule,
     FormsModule,
     ChartsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDatabaseService, { dataEncapsulation: false }
-    ),
     SmsClientModule, AppRoutingModule,
     RouterModule.forRoot(routes)
 
