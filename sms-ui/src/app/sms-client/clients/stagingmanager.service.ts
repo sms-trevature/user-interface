27121c5f6faaf8e3b3cee6143967ab18c3a37903
DebbieBiteye <debbiebiteye@gmail.com>
2019-04-25 15:45:35 -0400

    minor

---
+++ sms-ui/src/app/sms-client/clients/stagingmanager.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ManagerFeedback } from '../dto/ManagerFeedback';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StagingmanagerService {
+
+  /* injecting a service into the component */
+  constructor(private http: HttpClient) { }
+
+  private url= 'interview-service/interview/Feedback/InterviewId';
+
+  getMgrFeedback(): Observable<ManagerFeedback>
+}
+
