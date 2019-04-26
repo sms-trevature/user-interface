4f2fbbb1a90252903ca6395ad9fdec637cc7b063
Stephon Montgomery II <Stephonlm2@gmail.com>
2019-04-26 14:18:28 -0400

    fixed assign componenet it is able to be styled

--- sms-ui/src/app/components/survey/assign-survey/assign-survey.component.ts
+++ sms-ui/src/app/components/survey/assign-survey/assign-survey.component.ts
@@ -3,6 +3,7 @@ import { SurveyService } from 'src/app/sms-client/clients/survey.service';
 import { UsersClientService } from 'src/app/sms-client/clients/users-client.service';
 import { SurveyQuestionService } from 'src/app/sms-client/clients/surveyquestion.service';
 import { Survey } from 'src/app/sms-client/dto/Survey';
+import { User } from 'src/app/sms-client/dto/User';
 
 
 
@@ -15,6 +16,8 @@ import { Survey } from 'src/app/sms-client/dto/Survey';
 export class AssignSurveyComponent implements OnInit {
 
   listOfSurvey: Survey[];
+  surveyCreator: User;
+
 
   constructor(
     private surveyService: SurveyService,
@@ -23,6 +26,12 @@ export class AssignSurveyComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.userService.findByEmail('blake.kruppa@revature.com').subscribe(
+      creator => {
+        this.surveyCreator = creator;
+      }
+    );
+
     this.surveyService.findAll().subscribe(
       data => {
         this.listOfSurvey = data;
