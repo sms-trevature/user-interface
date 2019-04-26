59b7e355f12fa73c6e54fb909ccc03c547bc6998
Colton Stamey <coltonstamey1@gmail.com>
2019-04-26 16:36:07 -0400

    Almost done with survey-respondent component

--- sms-ui/src/app/components/survey/survey-respondents/survey-respondents.component.ts
+++ sms-ui/src/app/components/survey/survey-respondents/survey-respondents.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SurveyHistory } from 'src/app/sms-client/dto/SurveyHistory';
+import { SurveyHistoryService } from 'src/app/sms-client/clients/survey-history.serivce';
 
 @Component({
   selector: 'app-survey-respondents',
@@ -9,9 +10,26 @@ import { SurveyHistory } from 'src/app/sms-client/dto/SurveyHistory';
 export class SurveyRespondentsComponent implements OnInit {
 
   listOfSurvey: SurveyHistory[];
-  constructor() { }
+  constructor(private surveyHistoryService: SurveyHistoryService) { }
 
   ngOnInit() {
+    this.surveyHistoryService.findBySurveyId(4).subscribe(
+      data => {
+        // data[i].dateCreated = new Date(data[i].dateCreated);
+        // data[i].closingDate = new Date(data[i].closingDate);
+        this.listOfSurvey = data;
+      }
+    );
+  }
+
+  getSurveys(sId: number) {
+    this.surveyHistoryService.findBySurveyId(4).subscribe(
+      data => {
+        // data[i].dateCreated = new Date(data[i].dateCreated);
+        // data[i].closingDate = new Date(data[i].closingDate);
+        this.listOfSurvey = data;
+      }
+    );
   }
 
 }
