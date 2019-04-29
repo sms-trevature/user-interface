4001aef68b86d237d08efb2198839d62c5cbfec0
dxm3686 <daviddammyers@gmail.com>
2019-04-28 20:49:04 -0400

    DO THE THING

--- sms-ui/src/app/app.module.ts
+++ sms-ui/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { ProfileInfoComponent } from './components/profile-info/profile-info.com
 import { ReportsComponent } from './reports/reports.component';
 import { InterviewComponent } from './interview/interview.component';
 import { ManageComponent } from './manage/manage.component';
-import { SurveyComponent } from './survey/survey.component';
 import { MngrSubAssociatesComponent } from './mngr-sub-associates/mngr-sub-associates.component';
 import { SubMan2CohortComponent } from './sub-man2-cohort/sub-man2-cohort.component';
 import { FakeServiceComponent } from './fake-service/fake-service.component';
