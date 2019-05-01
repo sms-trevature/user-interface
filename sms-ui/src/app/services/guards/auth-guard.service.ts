faa47752b1025e84c1f1e917fb421ad9d7d6cf93
Robert Connell <robert.lel.connell@gmail.com>
2019-05-01 02:44:12 -0400

    auth guard update

--- sms-ui/src/app/services/guards/auth-guard.service.ts
+++ sms-ui/src/app/services/guards/auth-guard.service.ts
@@ -11,13 +11,10 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.cognito.getLocalStorage()!='') {
+    if (this.cognito.localStorage.length) {
         return true;
     }
-
-    // navigate to login page
     this.router.navigate(['/login']);
-    // you can save redirect url so after authing we can move them back to the page they requested
     return false;
   }
 
