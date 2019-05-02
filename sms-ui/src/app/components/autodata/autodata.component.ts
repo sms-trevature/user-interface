205bd2b8cd1ee99b544270b5d85982cf6dfc2249
debbiebiteye <debbiebiteye@gmail.com>
2019-05-01 21:55:04 -0400

    adding comments to my 3 reports

--- sms-ui/src/app/components/autodata/autodata.component.ts
+++ sms-ui/src/app/components/autodata/autodata.component.ts
@@ -24,28 +24,6 @@ export class AutodataComponent implements OnInit {
   AssociateInterviewCountsFilter: AssociateInterviewCount[];
 
   constructor(private http: HttpClient, private interview: InterviewService, private autodata: AutodataService) {
-
-    /*  this.http.get('interview-service/interview').toPromise().then(data => {
-        console.log("retrieved   " + data); 
-        console.log(data[1]);
-       console.log( data[1].associateEmail);
-      
-
-      let objIndex = 0;
-
-      while (data[objIndex] != null || data[objIndex] != undefined) {
-
-        this.filteredAssociates.push(data[objIndex]);
-
-        objIndex++;
-
-      }
-
-
-    }); */
-
-    // console.log(this.filteredAssociates[2].associateEmail);
-    //{{temp.filteredAssociates.associateEmail}}
   }
 
   get listFilter(): string {
--- sms-ui/src/app/components/daynotice-report/daynotice-report.component.ts
+++ sms-ui/src/app/components/daynotice-report/daynotice-report.component.ts
@@ -3,6 +3,12 @@ import { DaynoticeService } from 'src/app/services/daynotice.service';
 import { Router } from '@angular/router';
 import { DayNotice, ActualDayNotice } from 'src/app/sms-client/dto/DayNotice';
 
+/*the purpose of this component is to retrieve all of the 
+associates who had a 24 hour notice for their interview and display 
+that informatiopn on the application. 
+
+*/
+
 @Component({
   selector: 'app-daynotice-report',
   templateUrl: './daynotice-report.component.html',
@@ -16,6 +22,14 @@ export class DaynoticeReportComponent implements OnInit {
   boolFilterVar = 'all';
 
   constructor(private dayNotice: DaynoticeService) { }
+
+  /*we have getters and setters for the lists and the list filters so we
+  can easily use these functions without actually using the ()
+
+  we are are performing filters within the list that allows us to 
+  search within the list by name and by email.
+  */
+  
 
   get listFilter(): string {
     return this.listFilterVar;
@@ -36,9 +50,11 @@ export class DaynoticeReportComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    //calls this function when the component is loading
    this.get24Hr();
   }
-
+//retrieves all of the associates/interviews and sets that data into the dayNoticeList
   get24Hr() {
     this.dayNotice.get24HrNotice().subscribe((data: ActualDayNotice[]) => {
       this.dayNoticeList = data;
@@ -47,12 +63,17 @@ export class DaynoticeReportComponent implements OnInit {
     });
   }
 
-
+//this was my attempt at filtering the results by showing only the true values for dayNotice
+//it works but I went about it a different way but decided to keep the method in here just in case
   filterByTrue(): ActualDayNotice[] {
     return this.dayNoticeList.filter((randomV: ActualDayNotice) => (randomV.twentyFourAssoc) == false);
 
   }
 
+
+//performs the actual filtering for the table 
+//by name, by email
+// the drop down shown in the application filters the result set by true or false
   performFilter(filterBy: string): ActualDayNotice[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.dayNoticeList.filter((temp: ActualDayNotice) =>
